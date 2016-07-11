536c95feac5c25f2d2a4e9620382ff45b206da87
zljzlj1 <1964641139@qq.com>
2016-07-11 17:00:33 +0800

    二级菜单

---
+++ weixin/src/com/zlj/menu/cgcaga.java
@@ -0,0 +1,14 @@
+package com.zlj.menu;
+
+public class Menu {
+	//¶þ¼¶²Ëµ¥
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+}
